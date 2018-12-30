c1c9c64d6f74b26ba065b2d20f7d133c9ec80535
Kristin <kjb336@gmail.com>
2018-12-30 17:38:36 -0500

    Created compnents for the Routes

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const AddExpensePage = () => (
+    <div>
+        This is from my add component
+    </div>
+);
+export default AddExpensePage;
---
+++ src/components/HelpPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const HelpPage = () => (
+    <div>
+        This is from my help  component
+    </div>
+);
+
+export default HelpPage;
